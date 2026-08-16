78de23eb6fb0de36b86bdc8b42a43febd416653c
bigman79 <64438880+bigman79@users.noreply.github.com>
2026-08-15 17:18:05 -0700

    breaking things down

--- EngineLib/include/application.h
+++ EngineLib/include/application.h
@@ -18,11 +18,17 @@ namespace engine {
 
     class Application {
     public:
+        bool IsRunning() const { return m_Running; }
+        void RequestQuit() { m_Running = false; }
+
         virtual ~Application() = default;
         virtual AppResult Init(int argc, char** argv) = 0;
         virtual AppResult Update() = 0;
         virtual AppResult OnEvent(const Event& e) = 0;
         virtual void Shutdown(AppResult result) = 0;
+
+    protected:
+        bool m_Running = true;
     };
 
     // Game implements this factory; engine calls it.
--- Game/main.cpp
+++ Game/main.cpp
@@ -2,22 +2,30 @@
 // Created by treyo on 8/14/2026.
 //
 #include "include/application.h"
+#include "SDL3/SDL_log.h"
 
 class MyGame : public engine::Application {
 public:
     engine::AppResult Init(int argc, char** argv) override {
-        // set up game state
+        m_Running = true;
         return engine::AppResult::Continue;
     }
 
     engine::AppResult Update() override {
+        // If quit was requested, return Success to tell SDL to shut down
+        if (!m_Running)
+            return engine::AppResult::Success;
+
         // game logic + tell Renderer to draw
         return engine::AppResult::Continue;
     }
 
     engine::AppResult OnEvent(const engine::Event& e) override {
-        if (e.type == engine::EventType::Quit)
+        if (e.type == engine::EventType::Quit) {
+            SDL_Log("Quit event received");
+            RequestQuit();
             return engine::AppResult::Success;
+        }
         return engine::AppResult::Continue;
     }
 
